16bfd7e8514927bca2430e81fd00630aa9250526
Gabriel Oliveira <163311856+GabrielOiliveira@users.noreply.github.com>
2024-06-11 20:27:34 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,13 @@
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+# Imprima o tipo de dados dessas duas variáveis
+# Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+# Imprima a terceira variável e também o seu tipo
+
+numero1 = 5
+numero2 = 2
+
+print (type(numero1), type(numero2))
+
+numero3 = numero1/numero2
+
+print (type(numero3), numero3)
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,13 @@
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+texto ="o resultado é"
+valor1 = 10
+valor2 = 3.5
+valor3 = valor1 + valor2
+
+print(texto, type(texto))
+print(valor1, type(valor1))
+print(valor2, type(valor2))
+print(valor3, type(valor3))
