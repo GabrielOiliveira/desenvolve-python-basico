37bd49768c18d50bbffde0ea41ce7add7414b016
Gabriel Oliveira <163311856+GabrielOiliveira@users.noreply.github.com>
2024-10-06 22:21:03 -0300

    aula1_questao5.py

--- modulo4/aula1_questao5.py
+++ modulo4/aula1_questao5.py
@@ -1,8 +1,9 @@
-n = int(input('Digite a quantidade de respondentes da pesquisa: '))
-soma,controle = 0, n
+respondentes = int(input('Digite a quantidade de respondentes:'))
+soma = 0
+quantidade = respondentes
 
-while controle > 0:
-    soma += int(input('Digite o valor da idade: '))
-    controle -= 1
+while quantidade > 0:
+    soma += int(input('Digite a idade do respondente:'))
+    quantidade -= 1
 
-print(f'A media das idades é igual a {(soma/n):,.0f} anos.')
+print (f'A média das idades dos respondentes é de {(soma/respondentes):.0f} anos.')
