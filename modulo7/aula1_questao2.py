c6ae99ae65ee87df5521b37a1ca1c9f3142e89b1
Gabriel Oliveira <163311856+GabrielOiliveira@users.noreply.github.com>
2024-10-11 14:25:00 -0300

    aula1_questao2.py

--- modulo7/aula1_questao2.py
+++ modulo7/aula1_questao2.py
@@ -2,4 +2,4 @@ primeiro_nome = input("Digite seu primeiro nome: ")
 sobrenome = input("Digite seu sobrenome: ")
 
 nome_completo = primeiro_nome + " " + sobrenome
-print(f"Bem-vinda, {nome_completo}!")
+print(f"Bem-vindo, {nome_completo}!")
